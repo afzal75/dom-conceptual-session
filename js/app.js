53a5be5e3a794749cbadc425f265bf3c12ea829e
afzal75 <afzalbhuiyan16@gmail.com>
2022-02-09 06:18:24 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// const allButton =  document.getElementsByClassName('button-test');
+// const prime =  document.getElementById('prime');
+// console.log(allButton);
+// // console.log(prime);
+
+
+// for (const button of allButton){
+//     button.addEventListener('click', function(e){
+//         // console.log(e.target.parentNode);
+//         if(e.target.id == 'prime') {
+//             document.getElementById('title').style.color= 'red';
+//         }
+//         else{
+//             document.getElementById('title').style.color = 'green';
+//         }
+//     })  
+// }
+
